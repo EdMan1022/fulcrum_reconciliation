f721b736d15026fe25fe86cb117c32f20ceea75b
EdMan1022 <EdMan1022@gmail.com>
2018-02-06 11:15:03 -0500

    Removed unnecessary bad data compilation functions.

--- current_month.py
+++
@@ -1,7 +0,0 @@
-from .bad_data_compiler import fulcrum_bad_data_compiler
-import datetime
-
-today = datetime.datetime.today()
-
-this_month = today.date()
-fulcrum_bad_data_compiler(month=this_month)

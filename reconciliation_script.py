8bfc7714fed0d1123138cb237e0bbdc2eea91170
EdMan1022 <EdMan1022@gmail.com>
2018-02-06 12:02:59 -0500

    Added config directory

--- reconciliation_script.py
+++ reconciliation_script.py
@@ -7,9 +7,9 @@ env_path = os.path.join(os.getcwd(), '.env')
 dotenv.load_dotenv(env_path)
 
 # Set authorization key value for request headers
-API_KEY = os.environ.get('FULCRUM_SANDBOX_API_KEY')
+API_KEY = os.environ.get('FULCRUM_PRODUCTION_API_KEY')
 
-auto_reconciler = AutoReconciler(API_KEY)
+auto_reconciler = AutoReconciler(API_KEY, 'production')
 auto_reconciler.list_completed_surveys()
 auto_reconciler.good_ids_list = auto_reconciler.configure_good_ids_list()
 
